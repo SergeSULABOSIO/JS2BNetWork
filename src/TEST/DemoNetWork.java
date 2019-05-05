f08b4ac6c36c2b692730835972acd7d5983ae001
Serge SULA <sulabosiog@gmail.com>
2019-05-05 22:53:22 +0100

    RAS
    RAS

--- src/TEST/DemoNetWork.java
+++ src/TEST/DemoNetWork.java
@@ -12,12 +12,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  * @author user
  */
-public class Demo extends javax.swing.JFrame {
+public class DemoNetWork extends javax.swing.JFrame {
 
     /**
      * Creates new form Demo
      */
-    public Demo() {
+    public DemoNetWork() {
         initComponents();
     }
 
@@ -176,20 +176,21 @@ public class Demo extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoNetWork.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoNetWork.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoNetWork.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoNetWork.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Demo().setVisible(true);
+                new DemoNetWork().setVisible(true);
             }
         });
     }
